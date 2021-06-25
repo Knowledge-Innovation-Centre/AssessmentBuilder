a3793b98d778874471094b4c55a1fdef1e6d7c4d
jurejager <jure.jager@dewesoft.com>
2021-06-25 17:43:08 +0200

    Minor fix at score report

--- apprenticeship-online-assessment-tool.php
+++ apprenticeship-online-assessment-tool.php
@@ -53,7 +53,7 @@ final class Apprenticeship_Online_Assessment_Tool {
      *
      * @var string
      */
-    public $version = '1.4.6';
+    public $version = '1.4.7';
 
     /**
      * Holds various class instances
